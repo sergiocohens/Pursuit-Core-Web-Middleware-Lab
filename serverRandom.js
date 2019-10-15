609ef404aa0e4917f4a9f9f451d3f6ed42db5538
sergiocohens <sergiocohensalama@pursuit.org>
2019-10-15 19:36:06 -0400

    fix

--- serverRandom.js
+++ serverRandom.js
@@ -11,18 +11,20 @@ let array = []
 const generateSpread = (req, res, next) => {
   let num1 = req.query.floor
   let num2 = req.query.ceil
-  if (array !== []) {
-    for (let i = num1; i <= num2; i++) {
+
+  array = []
+
+  for (let i = num1; i <= num2; i++) {
       array.push(i)
     }
-  }
+
   next()
 }
 
 const returnRandPick = (req, res, nest) => {
-  console.log(array)
   let length = array.length
   let index = Math.floor(Math.random() * length)
+  
   res.send({
     status: 'success',
     range: `[${req.query.floor}, ${req.query.ceil}]`,
